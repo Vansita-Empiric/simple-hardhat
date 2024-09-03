5662cd197121e120292155988c40a5c2dcdaf6bc
VansitaYGajjar <vansita.g@empiricinfotech.com>
2024-09-03 16:31:26 +0530

    final deployment and frontend integration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const URL = process.env.URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
