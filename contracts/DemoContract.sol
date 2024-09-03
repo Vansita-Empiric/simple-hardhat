043cf1d2d6fc53ead96bb8857bf736ad83984980
VansitaYGajjar <vansita.g@empiricinfotech.com>
2024-09-03 16:14:43 +0530

    contract deployment successful

---
+++ contracts/DemoContract.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract DemoContract {
+    struct Memo {
+        string name;
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    Memo[] memos;
+
+    function buyChai(
+        string memory _name,
+        string memory _message
+    ) public payable {
+        require(msg.value > 0, "Amount is not set");
+        owner.transfer(msg.value);
+        memos.push(Memo(_name, _message, block.timestamp, msg.sender));
+    }
+
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+    function getInt() public pure returns (uint) {
+        return 29;
+    }
+
+    function getStr() public pure returns (string memory) {
+        return "Hello";
+    }
+}
